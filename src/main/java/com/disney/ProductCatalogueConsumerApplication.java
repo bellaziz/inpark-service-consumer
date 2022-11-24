2c635e65437015cba117ac6afaa0e64ba79e53d0
BELLAZIZ <amine.x.bellaziz.-nd@disney.com>
2022-11-24 12:39:20 +0100

    Initial commit

---
+++ src/main/java/com/disney/ProductCatalogueConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogueConsumerApplication.class, args);
+	}
+
+}
